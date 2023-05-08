0db12dcd6f06f5e22cb3190de6a6ab488c4c3aaf
charl graham <charl.graham7@outlook.com>
2023-05-08 13:55:42 +0200

    New changes to app

--- sqlapp/Services/ProductService.cs
+++ sqlapp/Services/ProductService.cs
@@ -5,22 +5,18 @@ namespace sqlapp.Services
 {
 
     // This service will interact with our Product data in the SQL database
-    public class ProductService
+    public class ProductService : IProductService
     {
-        private static string db_source = "appserver7023.database.windows.net";
-        private static string db_user = "userapp21";
-        private static string db_password = "Christian2070!";
-        private static string db_database = "appserver7023";
+        private readonly IConfiguration _configuration;
+
+        public ProductService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         private SqlConnection GetConnection()
         {
-            
-            var _builder = new SqlConnectionStringBuilder();
-            _builder.DataSource = db_source;
-            _builder.UserID = db_user;
-            _builder.Password = db_password;
-            _builder.InitialCatalog = db_database;
-            return new SqlConnection(_builder.ConnectionString);
+            return new SqlConnection(_configuration.GetConnectionString("SQLConnection"));
         }
         public List<Product> GetProducts()
         {
@@ -31,7 +27,7 @@ namespace sqlapp.Services
             _connection.Open();
 
             SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
-            
+
             using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
             {
                 while (_reader.Read())
